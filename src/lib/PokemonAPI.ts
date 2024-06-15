13a685a256347a8590509a2515f052fe31f3afd3
Stephen Meycelino Fransisco <stephenmeycelinowork88@gmail.com>
2024-06-15 22:35:43 +0700

    fix(env): change API URL to file env

--- src/lib/PokemonAPI.ts
+++ src/lib/PokemonAPI.ts
@@ -1,13 +1,13 @@
-const POKEAPI_URL = "https://pokeapi.co/api/v2/";
+const POKEAPI_BASE_URL = process.env.NEXT_PUBLIC_POKEAPI_BASE_URL;
 
 export async function getPokemonList() {
-  const response = await fetch(POKEAPI_URL + "pokemon?limit=151&offset=0");
+  const response = await fetch(`${POKEAPI_BASE_URL}pokemon?limit=151&offset=0`);
   const data = await response.json();
   return data.results;
 }
 
 export async function getPokemonListById(name: string) {
-  const response = await fetch(POKEAPI_URL + "pokemon/" + name);
+  const response = await fetch(`${POKEAPI_BASE_URL}pokemon/${name}`);
   const data = await response.json();
   return data;
 }
